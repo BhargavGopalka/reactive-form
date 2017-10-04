b878321aac22f03d7a285f57a5d42768c30190c3
BhargavGopalka <gopalka.bhargav@gmail.com>
2017-10-04 10:32:13 +0530

    small changes
    reformat

--- src/app/another-form/another-form.component.ts
+++ src/app/another-form/another-form.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup} from "@angular/forms";
-import { Http, Response } from "@angular/http";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Http, Response} from "@angular/http";
 
 @Component({
   selector: 'app-another-form',
@@ -13,7 +13,7 @@ export class AnotherFormComponent implements OnInit {
   numbers = [];
   detailForm: FormGroup;
 
-  constructor( private formbuilder: FormBuilder, private http: Http) {
+  constructor(private formbuilder: FormBuilder, private http: Http) {
     this.numbers = new Array(60).fill(1).map((x, i) => i + 1);
   }
 
@@ -31,26 +31,27 @@ export class AnotherFormComponent implements OnInit {
   // countryPopulation = '';
   countryName = '';
   cns = [];
-  searchCountry(){
+
+  searchCountry() {
     this.http.get('https://restcountries.eu/rest/v2/name/' + this.countryName)
       .subscribe((res: Response) => {
-      this.cns = res.json();
-      // console.log(infoCountry);
-      // for (let i = 0; i < infoCountry.length; i++){
-      //   this.countryCapital = infoCountry[i].capital;
-      //   this.countryRegion = infoCountry[i].region;
-      //   this.countryPopulation = infoCountry[i].population;
-      //   console.log(this.countryCapital, this.countryRegion, this.countryPopulation);
-      //   this.cns.push({cC : this.countryCapital, cR :  this.countryRegion, cP : this.countryPopulation});
+        this.cns = res.json();
+        // console.log(infoCountry);
+        // for (let i = 0; i < infoCountry.length; i++){
+        //   this.countryCapital = infoCountry[i].capital;
+        //   this.countryRegion = infoCountry[i].region;
+        //   this.countryPopulation = infoCountry[i].population;
+        //   console.log(this.countryCapital, this.countryRegion, this.countryPopulation);
+        //   this.cns.push({cC : this.countryCapital, cR :  this.countryRegion, cP : this.countryPopulation});
         console.log(this.cns);
 
       })
   }
 
-  addToTable(form: any){
+  addToTable(form: any) {
 
-  this.details.push(form.value);
-  this.detailForm.reset();
+    this.details.push(form.value);
+    this.detailForm.reset();
   }
 
 }
